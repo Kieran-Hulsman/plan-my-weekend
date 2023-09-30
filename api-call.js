e57466726e5a88e824337763502c4a19556c2a64
Kieran Hulsman <kghulsma@uwaterloo.ca>
2023-09-30 10:38:47 -0400

    metaphor working

--- api-call.js
+++ api-call.js
@@ -2,10 +2,20 @@
 // kieran hulsman
 // sep 30 2023
 
-// search
 const sdk = require('api')('@metaphorapi/v1.0#22sl31gln0t5di1');
-
 sdk.auth('0ff8e847-cf84-4932-ac1b-df666918053b'); // api key
-sdk.search({query: 'fun things to do in toronto', numResults: 10})
-  .then(({ data }) => console.log(data))
-  .catch(err => console.error(err));
+
+const city = 'toronto'
+
+sdk.search({query: `what is happening in ${city} this weekend` , numResults: 1})
+  .then(({ data }) => {
+    const website_id = data.results[0].id;
+    
+    sdk.getContents({ids: website_id})
+        .then(({ data }) => {
+            console.log(data.contents[0].extract);
+        })
+        .catch(err => console.error(err));
+
+  })
+  .catch(err => console.error(err))
